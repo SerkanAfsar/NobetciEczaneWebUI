e69fe08dda2d909107300b46b15531360bac15bc
CrimsonSky <serkan-afsar@hotmail.com>
2022-04-22 13:37:32 +0300

    Start Init Updated

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  // res.status(200).json({ name: 'John Doe' })
+  axios.get("https//www.ankatasimacilik.com").then(resp => {
+    return resp;
+  }).catch(err => {
+    console.log(err);
+  })
 }
