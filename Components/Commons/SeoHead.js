6b48497d5272a215a36fefb3d63570a9061becde
CrimsonSky <serkan-afsar@hotmail.com>
2022-04-28 19:43:25 +0300

    SeoHead Key Updated

--- Components/Commons/SeoHead.js
+++ Components/Commons/SeoHead.js
@@ -10,23 +10,23 @@ const SeoHead = ({ title, description, img }) => {
 
             <title>{title}</title>
 
-            <meta name="description" content={description} />
-            <meta name="robots" content="follow, index" />
-            <meta name="canonical" content={`${process.env.NEXT_PUBLIC_ABSOLUTE_PATH}${router.asPath}`} />
+            <meta name="description" content={description} key="description" />
+            <meta name="robots" content="follow, index" key="robots" />
+            <meta name="canonical" content={`${process.env.NEXT_PUBLIC_ABSOLUTE_PATH}${router.asPath}`} key="canonical" />
 
-            <meta property="og:locale" content="tr_TR" />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:url" content={`${process.env.NEXT_PUBLIC_ABSOLUTE_PATH}${router.asPath}`} />
-            <meta property="og:site_name" content="Nöbetçi Eczaneler" />
+            <meta property="og:locale" content="tr_TR" key="oglocale" />
+            <meta property="og:type" content="website" key="ogtype" />
+            <meta property="og:title" content={title} key="ogtitle" />
+            <meta property="og:description" content={description} key="ogdescription" />
+            <meta property="og:url" content={`${process.env.NEXT_PUBLIC_ABSOLUTE_PATH}${router.asPath}`} key="ogurl" />
+            <meta property="og:site_name" content="Nöbetçi Eczaneler" key="ogsitename" />
 
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:site" content="@nobetcieczanelernet" />
-            <meta name="twitter:creator" content="@nobetcieczanelernet" />
-            <meta name="theme-color" content="#343a40"></meta>
+            <meta name="twitter:card" content="summary" key="ogtwittercard" />
+            <meta name="twitter:title" content={title} key="twittertitle" />
+            <meta name="twitter:description" content={description} key="twitterdescription" />
+            <meta name="twitter:site" content="@nobetcieczanelernet" key="twittersite" />
+            <meta name="twitter:creator" content="@nobetcieczanelernet" key="twittercreator" />
+            <meta name="theme-color" content="#343a40" key="themecolor" />
         </Head>
     )
 }
